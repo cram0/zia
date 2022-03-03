5bfcc46a845b0047a83dadd5ba2131f771c5e103
KURILOV Konstantin <konstantin.kurilov@epitech.eu>
2022-03-03 18:21:22 +0100

    Make php compilable in Windows

--- modules/php/PhpCgi.cpp
+++ modules/php/PhpCgi.cpp
@@ -8,15 +8,19 @@
 #include "PhpCgi.hpp"
 #include "Request.hpp"
 
+#if(_WIN32)
+#include <io.h>
+#else
 #include <unistd.h>
-#include <stdio.h>
+#endif
+
+#include <cstdio>
 #include <fcntl.h>
-#include <string.h>
+#include <cstring>
 
 #include <sstream>
 
-PhpCgi::PhpCgi()
-{
+PhpCgi::PhpCgi() {
     type = ModuleType::PHP_CGI;
     name = "PhpCgiName";
     std::cout << "PhpCgi created" << std::endl;
@@ -51,7 +55,13 @@ void PhpCgi::receive(std::any payload, ModuleType sender)
     char buf[CHUNK_SIZE] = {0};
 
     FILE *f;
+
+#if(_WIN32)
+    f = _popen(std::string("php-cgi " + request.getFilePath()).c_str(), "r");
+#else
     f = popen(std::string("php-cgi " + request.getFilePath()).c_str(), "r");
+#endif
+
     pread_size = fread(buf, 1, CHUNK_SIZE, f);
     f_data += buf;
 
@@ -61,7 +71,12 @@ void PhpCgi::receive(std::any payload, ModuleType sender)
         f_data += buf;
     }
 
+#if(_WIN32)
+    _pclose(f);
+#else
     pclose(f);
+#endif
+
     request.setData(f_data);
     if (request.getSsl() == nullptr)
         getCore()->send(request, ModuleType::PHP_CGI, ModuleType::NETWORK);
